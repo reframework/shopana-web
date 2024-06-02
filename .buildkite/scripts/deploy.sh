d823c305912865b073d18ed9cb39e76a6ac54958
phl <philipp.sapronov@gmail.com>
2024-06-02 14:53:10 +0300

    fix

--- .buildkite/scripts/deploy.sh
+++ .buildkite/scripts/deploy.sh
@@ -2,15 +2,15 @@ set -eo pipefail
 
 echo "Shopana: $APP_ENV"
 
-mkdir -p "/root/apps/$APP_ENV/app"
-mkdir -p "/root/apps/$APP_ENV/public"
-mkdir -p "/root/apps/$APP_ENV/db-data" # TODO: create vanila db for prod
+mkdir -p "~/apps/$APP_ENV/app"
+mkdir -p "~/apps/$APP_ENV/public"
+mkdir -p "~/apps/$APP_ENV/db-data" # TODO: create vanila db for prod
 
 # cp -r ./.env "/apps/$APP_ENV/.env" TODO: inject variables
-cp -r docker-compose.yml "/root/apps/$APP_ENV/app/docker-compose.yml"
-cp -r nginx/nginx.conf "/root/apps/$APP_ENV/app/nginx.conf.template"
+cp -r docker-compose.yml "~/apps/$APP_ENV/app/docker-compose.yml"
+cp -r nginx/nginx.conf "~/apps/$APP_ENV/app/nginx.conf.template"
 
-cd /root/apps/$APP_ENV/app
+cd ~/apps/$APP_ENV/app
 
 echo $DOCKER_LOGIN_PASSWORD | docker login ghcr.io -u reframework-bot --password-stdin
 
