f6dba43ac2f9793617423a9011e31f760072c547
phl <philipp.sapronov@gmail.com>
2024-06-02 15:50:49 +0300

    fix

--- .buildkite/scripts/deploy.sh
+++ .buildkite/scripts/deploy.sh
@@ -6,12 +6,13 @@ mkdir -p "~/apps/$APP_ENV/app"
 mkdir -p "~/apps/$APP_ENV/public"
 mkdir -p "~/apps/$APP_ENV/db-data" # TODO: create vanila db for prod
 
-bash .buildkite/scripts/generate-env.sh
 
 cp -r docker-compose.yml "~/apps/$APP_ENV/app/docker-compose.yml"
 cp -r nginx/nginx.conf "~/apps/$APP_ENV/app/nginx.conf.template"
 
 cd "~/apps/$APP_ENV/app"
+
+bash .buildkite/scripts/generate-env.sh
 
 echo $DOCKER_LOGIN_PASSWORD | docker login ghcr.io -u reframework-bot --password-stdin
 
