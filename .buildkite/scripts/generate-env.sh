4577c3cd3aa5c825038fa555cab4c2fd156cfc4e
phl <philipp.sapronov@gmail.com>
2024-06-03 14:52:04 +0300

    fix

--- .buildkite/scripts/generate-env.sh
+++ .buildkite/scripts/generate-env.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
 
-# Path to the template .env file
-TEMPLATE_ENV_FILE="template.env"
+TEMPLATE_FILE="template.env"
+ENV_FILE=".env"
 
-# Path to the output .env file
-OUTPUT_ENV_FILE=".env"
+# check if .env file exists
+if [ ! -f "$ENV_FILE" ]; then
+    echo "Error: .env file not found!"
+    exit 1
+fi
+
+# Renave .env to template.env
+mv "$ENV_FILE" "$TEMPLATE_FILE"
 
 # Create or clear the output .env file
-> "$OUTPUT_ENV_FILE"
+> "$ENV_FILE"
 
 # Read the template .env file line by line
 while IFS= read -r line || [[ -n "$line" ]]; do
@@ -18,16 +24,16 @@ while IFS= read -r line || [[ -n "$line" ]]; do
         # Check if the variable is set in the environment
         if [ -n "${!VAR_NAME}" ]; then
             # Write the variable and its value to the output .env file
-            echo "$VAR_NAME=${!VAR_NAME}" >> "$OUTPUT_ENV_FILE"
+            echo "$VAR_NAME=${!VAR_NAME}" >> "$ENV_FILE"
         else
             # Write the original line to the output .env file
-            echo "$line" >> "$OUTPUT_ENV_FILE"
+            echo "$line" >> "$ENV_FILE"
         fi
     else
         # Write the original line (empty or comment) to the output .env file
-        echo "$line" >> "$OUTPUT_ENV_FILE"
+        echo "$line" >> "$ENV_FILE"
     fi
-done < "$TEMPLATE_ENV_FILE"
+done < "$TEMPLATE_FILE"
 
 echo "==============================="
 echo ".env file created successfully."
