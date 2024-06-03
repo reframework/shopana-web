e09fd3ba7bc8db4fd51504debeaaa5adf5777de6
phl <philipp.sapronov@gmail.com>
2024-06-03 14:46:51 +0300

    gen secrets

--- .buildkite/scripts/deploy.sh
+++ .buildkite/scripts/deploy.sh
@@ -6,17 +6,30 @@ DOCKER_PASSWORD=$(buildkite-agent secret get docker_login_password)
 
 echo "Building"
 
-mkdir -p "~/apps/$APP_ENV/app"
-mkdir -p "~/apps/$APP_ENV/public"
-mkdir -p "~/apps/$APP_ENV/db-data" # TODO: create vanila db for prod
+mkdir -p "$WORKDIR/app"
+mkdir -p "$WORKDIR/public"
+mkdir -p "$WORKDIR/db-data"
 
 bash .buildkite/scripts/generate-env.sh
 
-cp -r .env                 "~/apps/$APP_ENV/app/.env"
-cp -r docker-compose.yml   "~/apps/$APP_ENV/app/docker-compose.yml"
-cp -r nginx/nginx.conf     "~/apps/$APP_ENV/app/nginx.conf.template"
+# List of secret names
+secret_names=(
+  "cms_guset_jwt"
+  "cms_customer_jwt"
+  "cms_tenant_jwt"
+  "cms_db_dns"
+  "webapi_jwt_secret"
+  "webapi_db_dns"
+  "webclient_rollbar_server_token"
+)
 
-cd "~/apps/$APP_ENV/app"
+bash .buildkite/scripts/generate-secrets.sh "${secret_names[@]}"
+
+cp -r .env                 "$WORKDIR/app/.env"
+cp -r docker-compose.yml   "$WORKDIR/app/docker-compose.yml"
+cp -r nginx/nginx.conf     "$WORKDIR/app/nginx.conf.template"
+
+cd "$WORKDIR/app"
 
 echo $DOCKER_PASSWORD | docker login ghcr.io -u reframework-bot --password-stdin
 
---
+++ .buildkite/scripts/generate-secrets.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+create_docker_secret() {
+    local secret_name=$1
+    local secret_value=$2
+    echo "$secret_value" | docker secret create --label external=true "$secret_name" -
+}
+
+get_buildkite_secret() {
+    local secret_name=$1
+    local secret_value=$(buildkite-agent secret get "$secret_name")
+    echo "$secret_value"
+}
+
+if [ "$#" -lt 1 ]; then
+    echo "Usage: $0 <secret_name1> <secret_name2> ... <secret_nameN>"
+    exit 1
+fi
+
+for secret in "$@"; do
+    # Get the value from Buildkite agent
+    secret_value=$(get_buildkite_secret "$secret")
+
+    if [ -z "$secret_value" ]; then
+        echo "Buildkite secret $secret is not set. Skipping..."
+        continue
+    fi
+
+    create_docker_secret "$secret" "$secret_value"
+done
+
+echo "==============================="
+echo "Docker secrets creation process completed."
+echo "==============================="
